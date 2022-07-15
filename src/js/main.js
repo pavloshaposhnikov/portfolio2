e584a4cc835d95eea648e99d5b9dc34b94451202
pavelshaposhnikov <pashapasha2281@gmail.com>
2022-07-15 19:54:56 +0100

    first-screen upd

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 $(function () {
-    $("#included__header").load("/modules/header/index.html");
+    $("#included__header").load("./modules/header/index.html");
 });
+
+// <1024px - start adaptivity
+const adaptiveBreakpoint = $(window).width() > 1024;
+
+// =====================================================
+// Custom cursor
+
+const cursor = $(".cursor");
+
+if (adaptiveBreakpoint) {
+    onmousemove = function (e){
+        cursor.css({
+            'left': e.clientX,
+            'top': e.clientY,
+        });
+    };
+};
